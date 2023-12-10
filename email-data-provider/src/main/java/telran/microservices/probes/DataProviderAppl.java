3660c0997b331893870921a1363edfe31285b4c2
Yuragranovsky <yuragranovsky@gmail.com>
2023-12-10 15:56:11 +0200

    adds application properties with H2 configuration and application class

---
+++ email-data-provider/src/main/java/telran/microservices/probes/DataProviderAppl.java
@@ -0,0 +1,16 @@
+package telran.microservices.probes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"telran"})
+public class DataProviderAppl {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataProviderAppl.class, args);
+
+	}
+
+}
